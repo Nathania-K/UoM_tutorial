c2ab318195d3f1e00da34e93453e7f2f0c59e8eb
Nathania-K <nathania.kulkarni@rmh.nhs.uk>
2026-07-22 09:38:42 +0100

    corrected import from 'SeqKit2026nk.modules.logging_demo import logging_demo' to 'SeqKit2026nk.modules import logging_demo' from __main__.py

--- SeqKit2026nk/__main__.py
+++ SeqKit2026nk/__main__.py
@@ -1,9 +1,9 @@
 from SeqKit2026nk.logger import setup_logging
-from SeqKit2026nk.modules.logging_demo import logging_demo
+from SeqKit2026nk.modules import logging_demo
 
 def main():
     setup_logging()
-    logging_demo()
+    logging_demo.logging_demo()
 
 if __name__ == "__main__":
     main()
