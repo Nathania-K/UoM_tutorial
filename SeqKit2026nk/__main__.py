3591190511a221e13bbc51aaa9380574af4c8f39
Nathania-K <nathania.kulkarni@rmh.nhs.uk>
2026-07-22 09:29:25 +0100

    removed import logging from __main__.py

--- SeqKit2026nk/__main__.py
+++ SeqKit2026nk/__main__.py
@@ -1,5 +1,3 @@
-import logging.config
-from SeqKit2026nk.logging_config import LOG_CONFIG
 from SeqKit2026nk.logger import setup_logging
 from SeqKit2026nk.modules.logging_demo import logging_demo
 
